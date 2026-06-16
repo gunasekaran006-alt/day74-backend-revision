17528dd715dd5c0f6964be4ca54140cbb9d2a170
gunasekaran006-alt <gunasekaran006@gmail.com>
2026-06-16 22:39:34 +0530

    updates

---
+++ main.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+const dbConnection = require("./config/dbConnection.config");
+const productRoutes = require("./routes/products.routes");
+const osRoutes = require("./routes/system.routes");
+
+app.use(express.json());
+app.use("/api/products", productRoutes);
+app.use(osRoutes);
+
+dbConnection();
+app.listen(process.env.port, () => {
+    console.log(`Server up and running on ${process.env.port}`);
+});
